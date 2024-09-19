1d1a62ededcdb0e590b8f1c5781cdfb04ec4d1a5
kms5739 <37724041+kms5739@users.noreply.github.com>
2024-09-19 10:35:28 +0900

    0919 app.js main태그 내부로 routes 이동 / board 패키지 생성

--- travelnote_front/src/App.js
+++ travelnote_front/src/App.js
@@ -10,11 +10,11 @@ function App() {
     <div className="wrap">
       <Header />
       <main className="content">
-        <h1>Travel Note 메인 페이지</h1>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/foreign/*" element={<ForeignMain />}></Route>
+        </Routes>
       </main>
-      <Routes>
-        <Route path="/foreign/*" element={<ForeignMain />}></Route>
-      </Routes>
       <Footer />
     </div>
   );
