75f31099bab3567f750a31ef08fba86c813b02ec
Chae <chae02100103@gmail.com>
2024-09-20 17:15:21 +0900

    여행지 검색 수정

--- travelnote_front/src/component/foreignPlan/ForeignMain.js
+++ travelnote_front/src/component/foreignPlan/ForeignMain.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import "./foreignPlan.css";
 import ForeignList from "./ForeignList";
 import ItineraryForm from "./ItineraryForm";
+import ForeignPlanView from "./ForeignPlanView";
 
 const ForeignMain = () => {
   return (
@@ -14,6 +15,10 @@ const ForeignMain = () => {
         <Route
           path="createItinerary/:regionNo"
           element={<ItineraryForm />}
+        ></Route>
+        <Route
+          path="viewPlan/:itineraryNo"
+          element={<ForeignPlanView />}
         ></Route>
       </Routes>
     </>
---
+++ travelnote_front/src/component/foreignPlan/ForeignPlanView.js
@@ -0,0 +1,9 @@
+const ForeignPlanView = () => {
+  return (
+    <div className="section">
+      <div className="title">일정 보기</div>
+    </div>
+  );
+};
+
+export default ForeignPlanView;
