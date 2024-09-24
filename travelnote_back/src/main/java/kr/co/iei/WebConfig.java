c63678895ebf7483dc2f5d2bd380b12ec8742793
subin002003 <leesubin98619@gmail.com>
2024-09-24 09:56:27 +0900

    WebConfig 업데이트

--- travelnote_back/src/main/java/kr/co/iei/WebConfig.java
+++ travelnote_back/src/main/java/kr/co/iei/WebConfig.java
@@ -4,10 +4,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
 	@Value("${file.root}")
 	public String root;
 
@@ -15,7 +18,8 @@ public class WebConfig {
 	public BCryptPasswordEncoder bCrypt() {
 		return new BCryptPasswordEncoder();
 	}
-	
+
+	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry
 			.addResourceHandler("/product/editor/**")
